5105546aaf920a763e717f6015341e883f1b2358
Shahed_ali <shahed@beyondscale.tech>
2023-10-08 14:23:30 +0530

    Added routing to the Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./Components/Routes";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
