5a931c664e67ea722bd958af24ddd0385a77b97f
Shahed_ali <shahed@beyondscale.tech>
2023-10-08 22:04:29 +0530

    Completed books fetching, loading and page size change and pagination stuff, wired api and fixed bugs

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,90 @@
-import React from "react";
-import { Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import { Col, Pagination, Row } from "antd";
+import Search from "./Search";
+import Analytics from "./Analytics";
+import BooksList from "./Books/BooksList";
+import Settings from "./Settings";
+import Typography from "antd/es/typography/Typography";
 function Home() {
+  const [pageSize, setPageSize] = useState(10);
+  const [isLoading, setIsLoading] = useState(false);
+  const [booksList, setBooksList] = useState({ items: [] });
+  const [responseTime, setResponseTime] = useState(null);
+  const [pages, setPages] = useState({
+    currentPage: 1,
+    startIndex: 0,
+  });
   return (
-    <Row>
-      <Col>Hello</Col>
+    <Row gutter={[24, 24]}>
+      <Col span={12}>
+        <Analytics />
+      </Col>
+      <Col span={12}>
+        <Search
+          setBooksList={setBooksList}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          pages={pages}
+          pageSize={pageSize}
+          setPageSize={setPageSize}
+          setPages={setPages}
+          setResponseTime={setResponseTime}
+          responseTime={responseTime}
+        />
+      </Col>
+      {responseTime && (
+        <Col span={20} offset={2}>
+          <Typography.Text type="secondary">
+            Fetched in {responseTime / 1000} sec
+          </Typography.Text>
+        </Col>
+      )}
+      <Col span={20} offset={2}>
+        <BooksList
+          responseTime={responseTime}
+          booksList={booksList}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+        />
+      </Col>
+      {booksList.items.length > 0 && (
+        <Col style={{ textAlign: "right" }} span={24}>
+          <Pagination
+            showSizeChanger={true}
+            showQuickJumper={false}
+            pageSize={pageSize}
+            current={pages.currentPage}
+            total={booksList.totalItems}
+            pageSizeOptions={[5, 10, 20]}
+            onShowSizeChange={(cur, size) => {
+              setPageSize(size);
+              setPages({
+                ...pages,
+                startIndex:
+                  cur === 1
+                    ? 0
+                    : Math.min(
+                        (cur - 1) * pageSize,
+                        booksList.totalItems - pageSize
+                      ),
+              });
+            }}
+            onChange={(page, pageSize) => {
+              setPages({
+                ...pages,
+                currentPage: page,
+                startIndex:
+                  page === 1
+                    ? 0
+                    : Math.min(
+                        (page - 1) * pageSize,
+                        booksList.totalItems - pageSize
+                      ),
+              });
+            }}
+          />
+        </Col>
+      )}
     </Row>
   );
 }
--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,7 +1,74 @@
-import React from "react";
+import { AutoComplete, Input, Spin } from "antd";
+import React, { useEffect, useState } from "react";
+import { searchBooksAPI } from "../services/books.services";
 
-function Search() {
-  return <div>Search</div>;
+function Search({
+  width = "100%",
+  size = "large",
+  setBooksList,
+  isLoading,
+  setIsLoading,
+  pages,
+  pageSize,
+  setPageSize,
+  setPages,
+  setResponseTime,
+}) {
+  // const [isLoading, setIsLoading] = useState(false);
+  const [searchedVal, setSearchedVal] = useState("");
+  useEffect(() => {
+    if (searchedVal.length > 0) {
+      setIsLoading(true);
+      const timer = setTimeout(() => {
+        handleSearch();
+      }, 300);
+      return () => {
+        clearTimeout(timer);
+      };
+    } else {
+      setBooksList({ items: [] });
+      setPages({
+        currentPage: 1,
+        startIndex: 0,
+      });
+      setPageSize(10);
+    }
+  }, [searchedVal, pages, pageSize]);
+
+  async function handleSearch() {
+    try {
+      const booksData = await searchBooksAPI({
+        searchQuery: searchedVal,
+        startIndex: pages.startIndex,
+        maxResults: pageSize,
+      });
+      setBooksList(booksData.data.books);
+      setResponseTime(booksData.data.responseTimeInMs);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return (
+    <AutoComplete
+      style={{ width: width }}
+      onChange={(value) => setSearchedVal(value)}
+      value={searchedVal}
+    >
+      <Input.Search
+        allowClear
+        size={size}
+        placeholder="Search for anything"
+        suffix={
+          <Spin
+            size="small"
+            style={{ visibility: isLoading ? "visible" : "hidden" }}
+          />
+        }
+      />
+    </AutoComplete>
+  );
 }
 
 export default Search;
